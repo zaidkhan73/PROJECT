14f50764bc2b5b919dfa59f99235b6b8820bb151
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-11-17 21:58:12 +0530

    completed study material file frontend

---
+++ studyMaterial/app2.js
@@ -0,0 +1,7 @@
+
+let button_2 = document.querySelectorAll(".button-2")
+button_2.forEach(val=>{
+    val.addEventListener("click",()=>{
+        location.href ="index-3.html"
+    })
+})
--- studyMaterial/script.js
+++ studyMaterial/script.js
@@ -1,14 +1,32 @@
-// Select all buttons
-const buttons = document.querySelectorAll('.button');
-
-// Add hover animation using JavaScript
-buttons.forEach(button => {
-  button.addEventListener('mouseenter', () => {
-    button.style.transition = 'transform 0.3s ease';
-    button.style.transform = 'scale(1.2)';
-  });
-
-  button.addEventListener('mouseleave', () => {
-    button.style.transform = 'scale(1)';
-  });
+let navNonChosen = document.querySelectorAll(".nav-nonchosen")
+let navChosen = document.querySelector(".nav-chosen")
+navNonChosen.forEach(element => {
+    element.addEventListener("mouseover",()=>{
+        navChosen.style.scale="1"
+        navChosen.style.opacity ="1"
+    })
+    element.addEventListener("mouseout",()=>{
+        navChosen.style.scale="1.1"
+        navChosen.style.opacity ="0.5"
+    })
 });
+
+let buttons = document.querySelectorAll(".button-1")
+buttons[0].addEventListener("click",()=>{
+  console.log("l")
+  location.href = "index-2_1.html"
+})
+buttons[1].addEventListener("click",()=>{
+  console.log("l")
+  location.href = "index-2_2.html"
+})
+
+
+let button_2 = document.querySelectorAll(".button-2")
+button_2.forEach(val=>{
+    val.addEventListener("click",()=>{
+        location.href ="index-3.html"
+    })
+})
+
+
