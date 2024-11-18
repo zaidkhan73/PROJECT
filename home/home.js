f2b8654fe93bba7b33315bbd2ffb357972221619
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-11-18 15:39:10 +0530

    done with home hero section and many more

--- home/home.js
+++ home/home.js
@@ -10,3 +10,22 @@ navNonChosen.forEach(element => {
         navChosen.style.opacity ="0.5"
     })
 });
+
+let searchIcon = document.querySelector(".search-icon")
+let searchInput = document.querySelector(".search input")
+let searchbut = document.querySelector(".search-but")
+searchInput.addEventListener("mouseover",()=>{
+    searchIcon.style.borderTop = "2.4px solid #88C273"
+    searchbut.style.borderTop = "2.4px solid #88C273"
+    searchInput.style.borderTop = "2.4px solid #88C273"
+    searchIcon.style.borderLeft = "2.4px solid #88C273"
+    searchbut.style.borderRight = "2.4px solid #88C273"
+    searchIcon.style.borderBottom = "2.4px solid #88C273"
+    searchbut.style.borderBottom = "2.4px solid #88C273"
+    searchInput.style.borderBottom = "2.4px solid #88C273"
+})
+searchInput.addEventListener("mouseout",()=>{
+    searchIcon.style.border = "none"
+    searchInput.style.border = "none"
+    searchbut.style.border = "none"
+})
