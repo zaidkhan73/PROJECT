3ac0e8d6556dc09ea85edc38a34d9a6d1c4d6086
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-11-18 12:56:08 +0530

    footer is done so the overall theme is created

--- studyMaterial/app2.js
+++ studyMaterial/app2.js
@@ -1,3 +1,17 @@
+let navNonChosen = document.querySelectorAll(".nav-nonchosen")
+let navChosen = document.querySelector(".nav-chosen")
+navNonChosen.forEach(element => {
+    element.addEventListener("mouseover",()=>{
+        navChosen.style.scale="1"
+        navChosen.style.opacity ="1"
+    })
+    element.addEventListener("mouseout",()=>{
+        navChosen.style.scale="1.1"
+        navChosen.style.opacity ="0.5"
+    })
+});
+
+
 
 let button_2 = document.querySelectorAll(".button-2")
 button_2.forEach(val=>{
